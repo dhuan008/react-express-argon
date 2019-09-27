15e01cc62051bb10cb13ac0e628331f18d7a8e68
Ryan Kelley <rdkelley@users.noreply.github.com>
2019-09-27 10:16:16 -0700

    Remove accepted type for bodyparser

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(cors());
 
 // Morgan and bodyParser
 app.use(morgan("combined"));
-app.use(bodyParser.json({ type: ["json"] }));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
